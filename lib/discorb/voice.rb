89384355ce8e95af207c344cc88318c36faa2fbf
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2021-09-11 17:00:32 +0900

    WIP

--- lib/discorb/voice.rb
+++ lib/discorb/voice.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
+require "discorb"
 require_relative "voice/version"
+require_relative "voice/extend"
+require_relative "voice/core"
 
 module Discorb
   module Voice
     class Error < StandardError; end
-    # Your code goes here...
   end
 end
---
+++ lib/discorb/voice/extend.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+module Discorb::Voice
+  module ClientVoicePrepend
+    def initialize(...)
+      super
+      @voice_clients = Discorb::Dictionary.new
+      @voice_conditions = {}
+    end
+  end
+
+  module ClientVoiceInclude
+    def event_voice_server_update(data)
+      client = Discorb::Voice::Client.new(self, data)
+      @voice_clients[data[:guild_id]] = client
+      @voice_conditions[data[:guild_id]].signal client
+    end
+
+    def connect_to(channel)
+      Async do
+        send_gateway(4, **{
+                          "guild_id": channel.guild.id.to_s,
+                          "channel_id": channel.id.to_s,
+                          "self_mute": channel.guild.me.voice_state&.self_mute?,
+                          "self_deaf": channel.guild.me.voice_state&.self_deaf?,
+                        })
+        condition = Async::Condition.new
+        @voice_conditions[channel.guild.id.to_s] = condition
+        condition.wait
+      end
+    end
+  end
+end
+
+class Discorb::Client
+  attr_accessor :voice_clients
+
+  include Discorb::Voice::ClientVoiceInclude
+  prepend Discorb::Voice::ClientVoicePrepend
+end
